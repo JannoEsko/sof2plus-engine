501bfaa8040043846c163b17439e3d3d2d9326a7
Janno Esko <eskojanno@gmail.com>
2025-11-30 20:39:14 +0200

    fix: SmartDL - using strlen(refPaks) in strcat'ing refChecksums

--- code/server/sv_init.c
+++ code/server/sv_init.c
@@ -756,7 +756,7 @@ void SV_SpawnServer( char *server, qboolean killBots ) {
                         }
 
                         Q_strcat(refPaks, sizeof(refPaks), va("%s%s/%s", strlen(refPaks) > 0 ? " " : "", gamename, basename));
-                        Q_strcat(refChecksums, sizeof(refChecksums), va("%s%d", strlen(refPaks) > 0 ? " " : "", checksum));
+                        Q_strcat(refChecksums, sizeof(refChecksums), va("%s%d", strlen(refChecksums) > 0 ? " " : "", checksum));
                     }
 
                     token = strtok(NULL, " ");
