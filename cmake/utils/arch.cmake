563ff6b9b4533177ec69209c37137e25ddf3f3bf
Janno Esko <JannoEsko@users.noreply.github.com>
2025-12-04 11:13:17 +0200

    Change arch.cmake script to be valid also on older cmake versions

--- cmake/utils/arch.cmake
+++ cmake/utils/arch.cmake
@@ -5,15 +5,18 @@ if(NOT FORCE_32BIT)
     set(DETECT_ARCH_C ${CMAKE_BINARY_DIR}/detect_arch.c)
 
     file(WRITE ${DETECT_ARCH_C}
-    "#include \"${SOURCE_DIR}/qcommon/q_platform.h\"
-    #pragma message(\"@\" ARCH_STRING \"@\")
-    int main(void) { return 0; }
-    ")
+    "#include \"${CMAKE_SOURCE_DIR}/code/qcommon/q_platform.h\"\n"
+    "#pragma message(\"@\" ARCH_STRING \"@\")\n"
+    "int main(void) { return 0; }\n"
+    )
 
-    try_compile(COMPILE_SUCCESS
-        SOURCES ${DETECT_ARCH_C}
-        OUTPUT_VARIABLE COMPILE_OUTPUT)
-
+# Change due to older cmake / issue #21609
+    try_compile(
+        COMPILE_SUCCESS
+        ${CMAKE_BINARY_DIR}/arch_test
+        ${DETECT_ARCH_C}
+        OUTPUT_VARIABLE COMPILE_OUTPUT
+    )
     string(REGEX MATCH "^[^\@]+@([a-zA-Z0-9_]+)@.*$" HAVE_MATCH ${COMPILE_OUTPUT})
     set(ARCH ${CMAKE_MATCH_1})
 
